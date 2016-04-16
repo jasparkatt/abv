faabb5b12f8a1bacb78ec171e17ee06c9e1802f6
Grady Vickery <k.vickery@ymail.com>
2016-04-16 12:16:55 -0500

    Added files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,32 @@ var gulp = require('gulp');
 
 // include plug-ins
 var jshint = require('gulp-jshint'),
-	browserSync = require('browser-sync');
-
+	browserSync = require('browser-sync'),
+    reload      = browserSync.reload;
+    
 // js hint task
 gulp.task('jshint', function() {
 	gulp.src('./src/scripts/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 		});
-		
+
+
 //browser sync server
 gulp.task('browser-sync', function() {
     browserSync(['./src/**/*.css', './src/**/*.js', './src/**/*.html'], {
         server: {
-            baseDir: "./abv"
+            baseDir: "./"
         }
     });
-});		
+});	
+
+
+// Default task to be run with `gulp`
+gulp.task('default', ['jshint', 'browser-sync'], function () {
+    gulp.watch('../styles/*.css', function (file) {
+        if (file.type === "changed") {
+            reload(file.path);
+        }
+    });
+});
